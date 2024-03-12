d6d86209cbd904652eb6b073f9a38a62f00a197e
mani1232 <support@worldmandia.cc>
2024-03-12 21:02:44 +0100

    update kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     }
 }
 
@@ -13,7 +13,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("maven-publish")
     id("java")
-    kotlin("jvm") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
 }
 
 dependencies {
@@ -95,7 +95,7 @@ allprojects {
 
     dependencies {
         // Kotlin
-        implementation(kotlin("stdlib", version = "1.9.22"))
+        implementation(kotlin("stdlib", version = "1.9.23"))
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
 
         // Included in spigot jar, no need to move to implementation
